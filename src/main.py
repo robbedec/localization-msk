4c6a97ca072bad1414992e4698978d972302ff69
Robbe Decorte <robbe.decorte@ugent.be>
2022-05-25 17:13:10 +0200

    fix lines between rooms

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 
-from util import resize_with_aspectratio, vertices
+from util import resize_with_aspectratio, vertices, room_center_coords
 from detector import PaintingDetector
 from matcher import PaintingMatcher
 from localiser import Localiser
@@ -17,7 +17,10 @@ class Mode(Enum):
     FVECTOR = 1
     COMBINATION = 2
 
-def create_map(room_pred, plan, file_path,pixel_coord):
+DIFF_ROOM_COUNTER = 0
+CURRENT_ROOM = 'Z'
+
+def create_map(room_pred, plan, file_path, visited_rooms):
     # TODO: remove after room_pred are normalized.
     # Random percentages
     #room_pred = np.random.uniform(low=0, high=1, size=(len(vertices),))
@@ -40,40 +43,37 @@ def create_map(room_pred, plan, file_path,pixel_coord):
         poly_filled = cv2.fillPoly(mask, [points], col)
         mask = poly_filled
 
-
-
     blended_im = cv2.addWeighted(plan/255, 0.5, poly_filled/255, 0.5, 0)
 
-
-    highest_prob_index = np.argmax(np.array(room_pred))
     probs_indices_sorted = np.flip(np.argsort(np.array(room_pred)))
     pred_text = [f'Zaal: {vertices[probs_indices_sorted[i]]} ({round(room_pred[probs_indices_sorted[i]], 3)})' for i in range(3)]
     for i, text in enumerate(pred_text):
         cv2.putText(img=blended_im, text=text, org=(50, plan.shape[0] - 100 + (i * 35)), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(0, 255, 0), thickness=2)
 
-    kamer = vertices[highest_prob_index]
-    # print(kamer)
+    global CURRENT_ROOM, DIFF_ROOM_COUNTER
+    kamer = vertices[probs_indices_sorted[0]]
     
-    if main.oude_kamer != kamer:
-        main.buffer += 1
-        if main.buffer > 10:
-            main.oude_kamer = kamer
-            main.buffer = 0
-            for index, row in pixel_coord.iterrows():
-                kamernummer, x, y = row
-                if kamernummer == kamer:
-                    punt = (x,y)
-                    main.kamer_volgorde.append(punt)
-    for i in range(len(main.kamer_volgorde)-2 ):
-        cv2.line(blended_im, main.kamer_volgorde[i], main.kamer_volgorde[i+1], (255,0,0), 2, cv2.LINE_AA)
-    if(len(main.kamer_volgorde)>1):
-        cv2.arrowedLine(blended_im, main.kamer_volgorde[len(main.kamer_volgorde)-2], main.kamer_volgorde[len(main.kamer_volgorde)-1], (255,0,0), 2, cv2.LINE_AA)
+    # TODO: Dit kijkt als er 10x iets anders dan de huidige kamer voorspeld is.
+    # Als je 9x dezelfde voorspelling krijgt en dan voor de 10e een andere dan
+    # wordt de nieuwe kamer die dat slechts 1 keer gedetecteerd werd?
+    #
+    # Misschien beter om een frequentielijst bij te houden en pas de kamer te wisselen
+    # als dezelfde kamer 10x de hoogste probabiliteit heeft.
+    if CURRENT_ROOM != kamer:
+        DIFF_ROOM_COUNTER += 1
+        if DIFF_ROOM_COUNTER > 10:
+            CURRENT_ROOM = kamer
+            DIFF_ROOM_COUNTER = 0
+
+            visited_rooms.append(room_center_coords[kamer])
+
+    for i in range(len(visited_rooms) - 2):
+        cv2.line(blended_im, visited_rooms[i], visited_rooms[i+1], (255,0,0), 2, cv2.LINE_AA)
+
+    if(len(visited_rooms) > 1):
+        cv2.arrowedLine(blended_im, visited_rooms[len(visited_rooms) - 2], visited_rooms[len(visited_rooms)- 1], (255,0,0), 2, cv2.LINE_AA)
 
     cv2.imshow('HMM Visualization', blended_im)
-
-    # poly_fill contains the raw mask
-    #cv2.imshow('poly_fill', mask)
-    #cv2.waitKey(0)
 
 def main():
     if len(sys.argv) != 7:
@@ -91,9 +91,9 @@ def main():
     is_gopro = False
 
     # Matching mode
-    #mode = Mode.ORB.value
+    mode = Mode.ORB.value
     #mode = Mode.FVECTOR.value
-    mode = Mode.COMBINATION.value
+    #mode = Mode.COMBINATION.value
 
     # Video setup and properties
     cap = cv2.VideoCapture(video_path)
@@ -108,17 +108,16 @@ def main():
     matcher = PaintingMatcher(csv_path, database_file,features=100,mode=mode)
     localiser = Localiser(matcher=matcher, hmm_distribution='gaussian')
 
+    # For map visualization
     map_img = cv2.imread(map_path)
-    pixel_coord = pd.read_csv("src/data/coord.csv")
-
-    main.oude_kamer = 'Z'
-    main.buffer = 0
-    main.kamer_volgorde = []
+    visited_rooms = []
 
     cv2.namedWindow('Video')
 
     while True:
         success, img = cap.read()
+
+        print(CURRENT_ROOM, DIFF_ROOM_COUNTER, visited_rooms)
 
         if not success:
             break
@@ -136,12 +135,10 @@ def main():
             contour_results, img_with_contours = detector.contours(display=False)
 
             room_prediction = localiser.localise(img, contour_results, display=False)
-            #txt = "Zaal: " + room_prediction
-            #cv2.putText(img=img_with_contours, text=txt, org=(50, 250), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(0, 255, 0), thickness=2)
             cv2.imshow('Video', img_with_contours)
 
             # Visualize output of the hidden markov model.
-            create_map(localiser.prob_array, map_img.copy() , map_contour_file, pixel_coord)
+            create_map(localiser.prob_array, map_img.copy() , map_contour_file, visited_rooms)
 
         k = cv2.waitKey(int(1000 / fps / 1.5))
         if k != -1:
