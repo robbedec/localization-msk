c45c2cc8023a5a3c1a6ba2deb459a273bee01967
bendhaen <Benoit.DHaene@UGent.be>
2022-05-25 14:47:44 +0200

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -11,20 +11,22 @@ from localiser import Localiser
 from preprocessing import FrameProcessor
 from enum import Enum
 
+
 class Mode(Enum):
     ORB = 0
     FVECTOR = 1
     COMBINATION = 2
 
-def create_map(room_pred, plan, file_path):
+def create_map(room_pred, plan, file_path,pixel_coord):
     # TODO: remove after room_pred are normalized.
     # Random percentages
     #room_pred = np.random.uniform(low=0, high=1, size=(len(vertices),))
 
     # Load contour data from storage
     df_poly = pd.DataFrame(data=np.load(file_path, allow_pickle=True), columns=['polygon'])
-
     mask = np.zeros(plan.shape, dtype=np.uint8)
+
+
     for i, row in df_poly.iterrows():
         points = np.array(row['polygon'])
 
@@ -38,16 +40,34 @@ def create_map(room_pred, plan, file_path):
         poly_filled = cv2.fillPoly(mask, [points], col)
         mask = poly_filled
 
+
+
     blended_im = cv2.addWeighted(plan/255, 0.5, poly_filled/255, 0.5, 0)
 
 
-    #highest_prob_index = np.argmax(np.array(room_pred))
+    highest_prob_index = np.argmax(np.array(room_pred))
     probs_indices_sorted = np.flip(np.argsort(np.array(room_pred)))
     pred_text = [f'Zaal: {vertices[probs_indices_sorted[i]]} ({round(room_pred[probs_indices_sorted[i]], 3)})' for i in range(3)]
     for i, text in enumerate(pred_text):
         cv2.putText(img=blended_im, text=text, org=(50, plan.shape[0] - 100 + (i * 35)), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(0, 255, 0), thickness=2)
 
-
+    kamer = vertices[highest_prob_index]
+    # print(kamer)
+    
+    if main.oude_kamer != kamer:
+        main.buffer += 1
+        if main.buffer > 10:
+            main.oude_kamer = kamer
+            main.buffer = 0
+            for index, row in pixel_coord.iterrows():
+                kamernummer, x, y = row
+                if kamernummer == kamer:
+                    punt = (x,y)
+                    main.kamer_volgorde.append(punt)
+    for i in range(len(main.kamer_volgorde)-2 ):
+        cv2.line(blended_im, main.kamer_volgorde[i], main.kamer_volgorde[i+1], (255,0,0), 2, cv2.LINE_AA)
+    if(len(main.kamer_volgorde)>1):
+        cv2.arrowedLine(blended_im, main.kamer_volgorde[len(main.kamer_volgorde)-2], main.kamer_volgorde[len(main.kamer_volgorde)-1], (255,0,0), 2, cv2.LINE_AA)
 
     cv2.imshow('HMM Visualization', blended_im)
 
@@ -69,8 +89,6 @@ def main():
     map_contour_file = sys.argv[6]
 
     is_gopro = False
-    
-
 
     # Matching mode
     #mode = Mode.ORB.value
@@ -91,6 +109,11 @@ def main():
     localiser = Localiser(matcher=matcher, hmm_distribution='gaussian')
 
     map_img = cv2.imread(map_path)
+    pixel_coord = pd.read_csv("src/data/coord.csv")
+
+    main.oude_kamer = 'Z'
+    main.buffer = 0
+    main.kamer_volgorde = []
 
     cv2.namedWindow('Video')
 
@@ -118,7 +141,7 @@ def main():
             cv2.imshow('Video', img_with_contours)
 
             # Visualize output of the hidden markov model.
-            create_map(localiser.prob_array, map_img.copy(), map_contour_file)
+            create_map(localiser.prob_array, map_img.copy() , map_contour_file, pixel_coord)
 
         k = cv2.waitKey(int(1000 / fps / 1.5))
         if k != -1:
